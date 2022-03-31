39ad65c19dd5a8ee5ef4331fa25e86348d24d61f
Talha2000 <amjad_maqsood90@hotmail.com>
2022-03-30 22:03:15 -0400

    Changing file names and fixing small issues

--- FormEntry.Designer.cs
+++ FormEntry.Designer.cs
@@ -101,6 +101,7 @@
             this.button3.TabIndex = 6;
             this.button3.Text = "Log Out";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button5
             // 
